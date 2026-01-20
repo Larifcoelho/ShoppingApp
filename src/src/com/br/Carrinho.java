52880599c61a541815f7cacda0108ad74d1e67cf
Larifcoelho <larissafcoelho9@gmail.com>
2026-01-20 16:32:19 -0300

    updates

--- src/src/com/br/Carrinho.java
+++ src/src/com/br/Carrinho.java
@@ -1,7 +1,22 @@
 package src.com.br;
 
+import java.util.ArrayList;
+
 public class Carrinho {
 
-    private String Camisa;
-    private String calca
+    private ArrayList<Produto> lista = new ArrayList<>();
+
+    public void adicionarProduto(Produto produto) {
+       if (produto == null){
+           throw new IllegalArgumentException("Inválido. Digite o valor correto.");
+       }
+    lista.add (produto);
 }
+    public double calcularTotal(){
+        double total = 0;
+        for (Produto p : lista){
+            total += p.getPreco();
+        } return total;
+    }
+    }
+
--- src/src/com/br/Produtos.java
+++
@@ -1,5 +0,0 @@
-package src.com.br;
-
-public class Produtos {
-
-}
